e47768defe8766d8e0842b827b6abe25c03b6066
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-18 00:52:49 -0400

    Q2

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -3,8 +3,25 @@ const FROGS = 3;
 
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
 
+for (let sectionCount = 1; sectionCount <= FROGS; sectionCount++) {
+  let lane = document.createElement("li");
+  track.appendChild(lane);
+
+  let number = document.createElement("span");
+  number.innerText = sectionCount;
+  lane.appendChild(number);
+
+  lane.id = "frog's" + "" + sectionCount;
+}
+
+let froggy = [];
 // 2. Create li
 
+for (let x = 0; x < FROGS; x++) {
+  const newFrog = frogStable[x];
+  racers.push(newFrog);
+}
+console.log(froggy);
 // 3. Create span and add it to the li
 
 // 4. Assign an id to each lane
